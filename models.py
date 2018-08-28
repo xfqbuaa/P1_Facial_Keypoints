0591dd9f6f4af6e032f14e547b43ede401e77d46
xfqbuaa <xfqbuaa@gmail.com>
2018-08-28 11:12:03 +0800

    feat: Upload project files in submission commit
    Upload P1_Facial_Keypoints, which has been reviewed.
    There are no trained model.
    Default README.md

---
+++ models.py
@@ -0,0 +1,90 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+
+        # LeNet
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*53*53, 3000)
+        self.fc2 = nn.Linear(3000, 1000)
+        self.fc3 = nn.Linear(1000, 136)
+
+        # NaimishNet
+        #self.conv1 = nn.Conv2d(1, 32, 4)
+        #self.pool = nn.MaxPool2d(2, 2)
+        #self.drop1 = nn.Dropout(p=0.1)
+
+        #self.conv2 = nn.Conv2d(32, 64, 3)
+        #self.drop2 = nn.Dropout(p=0.2)
+
+        #self.conv3 = nn.Conv2d(64, 128, 2)
+        #self.drop3 = nn.Dropout(p=0.3)
+     
+        #self.conv4 = nn.Conv2d(128, 256, 1)
+        #self.drop4 = nn.Dropout(p=0.4)
+
+        #self.fc1 = nn.Linear(256*13*13, 5000)
+        #self.drop5 = nn.Dropout(p=0.5)
+        #self.fc2 = nn.Linear(5000, 1000)
+        #self.drop6 = nn.Dropout(p=0.6)
+        #self.fc3 = nn.Linear(1000, 136)
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+
+        # Lenet
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        # NaimishNet
+        #x = self.pool(F.relu(self.conv1(x)))
+        #x = self.drop1(x)
+
+        #x = self.pool(F.relu(self.conv2(x)))
+        #x = self.drop2(x)
+
+        #x = self.pool(F.relu(self.conv3(x)))
+        #x = self.drop3(x)
+
+        #x = self.pool(F.relu(self.conv4(x)))
+        #x = self.drop4(x)
+
+        #x = x.view(x.size(0), -1)
+        #x = F.relu(self.fc1(x))
+        #x = self.drop5(x)
+        #x = F.relu(self.fc2(x))
+        #x = self.drop6(x)
+        #x = self.fc3(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
